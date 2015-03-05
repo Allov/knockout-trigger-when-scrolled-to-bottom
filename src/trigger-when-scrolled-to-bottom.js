87a32954566f526fe7a3faaa1b61a2e48a8a8b63
Allov <frederik.charest@gmail.com>
2015-03-05 11:35:32 -0500

    added support for parent element

--- src/trigger-when-scrolled-to-bottom.js
+++ src/trigger-when-scrolled-to-bottom.js
@@ -1,80 +1,91 @@
-define(['knockout', 'jquery'], function(ko, $) {
-    'use strict';
-    //TODO: http://stackoverflow.com/questions/10324240/knockout-binding-handler-teardown-function (voir 2ieme réponse..?) (sur tous les bindings)
-    ko.bindingHandlers.triggerWhenScrolledToBottom = {
+define(['knockout', 'jquery'],
+    function(ko, $) {
+        'use strict';
 
-        //ko.utils.domData.get(element, key)
-        //ko.utils.domData.set(element, key, value)
-        //ko.utils.domData.clear(element)
-        init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
-            ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-                unregisterScrollEventIfRegistered(element);
-            });
+        var handlerId = 0;
 
-            var action = ko.utils.unwrapObservable(valueAccessor());
-            var options = getOptions(allBindingsAccessor);
+        //TODO: http://stackoverflow.com/questions/10324240/knockout-binding-handler-teardown-function (voir 2ieme réponse..?) (sur tous les bindings)
+        ko.bindingHandlers.triggerWhenScrolledToBottom = {
 
-            if (!ko.utils.unwrapObservable(options.disabled) && isScrolledIntoView(element, options)) {
-                executeAction(element, viewModel, action);
-            }
-        },
+            //ko.utils.domData.get(element, key)
+            //ko.utils.domData.set(element, key, value)
+            //ko.utils.domData.clear(element)
+            init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+                var action = ko.utils.unwrapObservable(valueAccessor());
+                var options = getOptions(allBindingsAccessor);
 
-        update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
-            var action = ko.utils.unwrapObservable(valueAccessor());
-            var options = getOptions(allBindingsAccessor);
+                ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+                    unregisterScrollEventIfRegistered(element, options);
+                });
 
-            if (ko.utils.unwrapObservable(options.disabled)) {
-                unregisterScrollEventIfRegistered(element);
-            } else {
-                registerScrollEventIfNotAlreadyRegistered(element, action, options, viewModel);
-            }
-        }
-    };
-
-    //TODO: debounce!!!
-    function registerScrollEventIfNotAlreadyRegistered(element, action, options, viewModel) {
-        var isRegistered = ko.utils.domData.get(element, 'scrollHandler');
-
-        if (!isRegistered) {
-            ko.utils.domData.set(element, 'scrollHandler', true);
-
-            $(window).on('scroll.ko.scrollHandler', function(data, event) {
-                if (isScrolledIntoView(element, options)) {
-                    executeAction(element, viewModel, action, data, event);
+                if (!ko.utils.unwrapObservable(options.disabled) && isScrolledIntoView(element, options)) {
+                    executeAction(element, viewModel, action);
                 }
-            });
+            },
+
+            update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+                var action = ko.utils.unwrapObservable(valueAccessor());
+                var options = getOptions(allBindingsAccessor);
+
+                if (ko.utils.unwrapObservable(options.disabled)) {
+                    unregisterScrollEventIfRegistered(element, options);
+                } else {
+                    registerScrollEventIfNotAlreadyRegistered(element, action, options, viewModel);
+                }
+            }
+        };
+
+        //TODO: debounce!!!
+        function registerScrollEventIfNotAlreadyRegistered(element, action, options, viewModel) {
+            var isRegistered = ko.utils.domData.get(element, options.handler);
+
+            if (!isRegistered) {
+                ko.utils.domData.set(element, options.handler, true);
+
+                $(options.useParent ? $(element).parent() : window).on('scroll.ko.' + options.handler, function(data, event) {
+                    if (isScrolledIntoView(element, options)) {
+                        executeAction(element, viewModel, action, data, event);
+                    }
+                });
+            }
         }
-    }
 
-    function getOptions(allBindingsAccessor){
-        return $.extend({
-            disabled: false
-        }, allBindingsAccessor().triggerWhenScrolledToBottomOptions);
-    }
-
-    function unregisterScrollEventIfRegistered(element) {
-        var isRegistered = ko.utils.domData.get(element, 'scrollHandler');
-
-        if (isRegistered) {
-            ko.utils.domData.set(element, 'scrollHandler', null);
-            $(window).off('scroll.ko.scrollHandler');
+        function getOptions(allBindingsAccessor) {
+            return $.extend({
+                disabled: false,
+                useParent: false,
+                handler: 'scrollHandler' + (++handlerId)
+            }, allBindingsAccessor().triggerWhenScrolledToBottomOptions);
         }
-    }
 
-    function executeAction(element, viewModel, action, data, event){
-        if(typeof action === 'string' || action instanceof String){
-            $(element).trigger(action);
-        }else{
-            action.call(viewModel, data, event);
+        function unregisterScrollEventIfRegistered(element, options) {
+            var isRegistered = ko.utils.domData.get(element, options.handler);
+
+            if (isRegistered) {
+                ko.utils.domData.set(element, options.handler, null);
+                $(options.useParent ? $(element).parent() : window).off('scroll.ko.' + options.handler);
+            }
         }
-    }
 
-    function isScrolledIntoView(elem, options) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height() + (ko.utils.unwrapObservable(options.offset) || 0);
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
+        function executeAction(element, viewModel, action, data, event) {
+            if (typeof action === 'string' || action instanceof String) {
+                $(element).trigger(action);
+            } else {
+                action.call(viewModel, data, event);
+            }
+        }
 
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-});
+        function isScrolledIntoView(element, options) {
+            if (options.useParent) {
+                var $parent = $(element).parent();
+                return $parent.scrollTop() + $parent.innerHeight() >= $parent[0].scrollHeight;
+            } else {
+                var docViewTop = $(window).scrollTop();
+                var docViewBottom = docViewTop + $(window).height() + (ko.utils.unwrapObservable(options.offset) || 0);
+                var elemTop = $(element).offset().top;
+                var elemBottom = elemTop + $(element).height();
+
+                return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+            }
+        }
+    });
